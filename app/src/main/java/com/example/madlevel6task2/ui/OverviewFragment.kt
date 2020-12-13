369418e4b910b1162cf9cf6446cc578a7dde3cda
ArieBisfki <arie_bisfki@live.nl>
2020-12-13 17:03:02 +0100

    ui files renamed + overview fragment top elements

--- app/src/main/java/com/example/madlevel6task2/ui/OverviewFragment.kt
+++ app/src/main/java/com/example/madlevel6task2/ui/OverviewFragment.kt
@@ -12,21 +12,17 @@ import com.example.madlevel6task2.R
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class OverviewFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_overview, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 }
